029abae495ed72bcbea7f5952fbb86799a30808b
peyman <peymangitonly@gmail.com>
2024-10-03 13:51:06 +0000

    some changes

--- WebCam/post.php
+++ WebCam/post.php
@@ -3,8 +3,6 @@
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
 
-
-
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
 $fp = fopen( '../images/cam'.$date.'.png', 'wb' );
